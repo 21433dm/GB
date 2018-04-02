dd9855ab8236f9dafe943fcbacff3428dca5a5e9
David Makanjuola <21433dm@gmail.com>
2018-04-02 08:04:06 +0100

    Add files via upload

---
+++ app/Models/Status.php
@@ -0,0 +1,29 @@
+<?php 
+
+namespace Greenbook\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Status extends Model
+{
+    protected $table = 'statuses';
+
+    protected $fillable = [
+        'body'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo('Greenbook\Models\User', 'user_id');
+    }
+
+    public function scopeNotReply($query)
+    {
+        return $query->whereNull('parent_id');
+    }
+
+    public function replies()
+    {
+        return $this->hasMany('Greenbook\Models\Status', 'parent_id');
+    }
+}
